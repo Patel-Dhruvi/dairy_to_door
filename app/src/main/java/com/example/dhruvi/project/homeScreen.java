05156fa41f7a21a8b9aee80558ed22ace79efaf5
baisaaRules <khyatichavda25@gmail.com>
2019-01-02 00:59:54 -0800

    today work

--- app/src/main/java/com/example/dhruvi/project/homeScreen.java
+++ app/src/main/java/com/example/dhruvi/project/homeScreen.java
@@ -16,7 +16,7 @@ public class homeScreen extends AppCompatActivity {
 
 
     private static int currentPage = 0;
-    private static int  NUM_PAGES=0;
+    private static int  NUM_PAGES=5;
     Timer timer;
     int IMAGES[] ={R.drawable.amul_cow_milk,R.drawable.amul_deshi,R.drawable.amul_diamond,R.drawable.amul_gold,R.drawable.amul_shakti,R.drawable.amul_taaza};
 //    private ArrayList<Integer> ImageArray = new ArrayList<Integer>();
@@ -48,7 +48,7 @@ public class homeScreen extends AppCompatActivity {
         final Runnable Update = new Runnable() {
             public void run() {
 //                int NUM_PAGES = 0;
-                if (currentPage == NUM_PAGES-1) {
+                if (currentPage == NUM_PAGES) {
                     currentPage = 0;
                 }
                 mViewpager.setCurrentItem(currentPage++, true);
